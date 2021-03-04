5af30f66c63e192da5cdd5a665d0e1b6b94e0c1c
Nicolas Schmidt <nschmidtg@gmail.com>
2021-03-04 13:29:12 +0100

    chore(dataloader): midified podcastmix dataloader to mix on the fly

---
+++ PodcastMixNew.py
@@ -0,0 +1,112 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+import pandas as pd
+import soundfile as sf
+import os
+import numpy as np
+import random
+
+class PodcastMix(Dataset):
+    """Dataset class for PodcastMix source separation tasks.
+    This dataset created Podcasts-like mixes on the fly consisting in
+    2 sources: background music and foreground speech
+    Args:
+        csv_dir (str): The path to the metadata files: speech.csv and music.csv
+        sample_rate (int) : The sample rate of the sources and mixtures.
+        segment (int) : The desired sources and mixtures length in s.
+    References
+        [1] "Jamendo....
+        [2] "VCTK...
+    """
+
+    dataset_name = "PodcastMix"
+    def __init__(self, csv_dir, sample_rate=44100, segment=3, return_id=False):
+        self.csv_dir = csv_dir
+        self.return_id = return_id
+        self.speech_csv_path = os.path.join(self.csv_dir, 'speech.csv')
+        self.music_csv_path = os.path.join(self.csv_dir, 'music.csv')
+        self.segment = segment
+        self.sample_rate = sample_rate
+        # Open csv files
+        self.df_speech = pd.read_csv(self.speech_csv_path, engine='python')
+        self.df_music = pd.read_csv(self.music_csv_path, engine='python')
+        # Get rid of the utterances too short
+        if self.segment is not None:
+            max_len = len(self.df_music)
+            self.seg_len = int(self.segment * self.sample_rate)
+            # Ignore the file shorter than the desired_length
+            self.df_music = self.df_music[self.df_music["length"] >= self.seg_len]
+            print(
+                f"Music: Drop {max_len - len(self.df_music)} utterances from {max_len} "
+                f"(shorter than {segment} seconds)"
+            )
+
+            max_len = len(self.df_speech)
+            self.seg_len = int(self.segment * self.sample_rate)
+            # Ignore the file shorter than the desired_length
+            self.df_speech = self.df_speech[self.df_speech["length"] >= self.seg_len]
+            print(
+                f"Speech: Drop {max_len - len(self.df_speech)} utterances from {max_len} "
+                f"(shorter than {segment} seconds)"
+            )
+        else:
+            self.seg_len = None
+
+    def __len__(self):
+        # for now, its a full permutation
+        return len(self.df_music) * len(self.df_speech)
+    
+    def compute_rand_start_end(self, row):
+        start = stop = 0
+        if self.seg_len is not None:
+            start = random.randint(0, row["length"] - self.seg_len)
+            stop = start + self.seg_len
+        else:
+            start = 0
+            stop = None
+        return start, stop
+
+    def __getitem__(self, idx):
+        speech_idx = idx // len(self.df_music)
+        music_idx = idx % len(self.df_music)
+        # Get the row in speech dataframe
+        row_speech = self.df_speech.iloc[speech_idx]
+        row_music = self.df_music.iloc[music_idx]
+        
+        sources_list = []
+        # If there is a seg, start point is set randomly
+        start, stop = self.compute_rand_start_end(row_speech)
+        # We want to cleanly separate Speech, so its the first source in the sources_list
+        source_path = row_speech["speech_path"]
+        s_speech, _ = sf.read(source_path, dtype="float32", start=start, stop=stop, samplerate = self.sample_rate)
+        s_speech = s_speech / max(s_speech)
+        sources_list.append(s_speech)
+
+        # now for music:
+        start, stop = self.compute_rand_start_end(row_music)
+        source_path = row_music["music_path"]
+        s_music, _ = sf.read(source_path, dtype="float32", start=start, stop=stop, samplerate = self.sample_rate)
+        # normalize:
+        s_music = s_music / max(s_music)
+        sources_list.append(s_music)
+
+        # compute the mixture
+        mixture = s_music * 0.2 + s_speech
+        # Convert to torch tensor
+        mixture = torch.from_numpy(mixture)
+        # Stack sources
+        sources = np.vstack(sources_list)
+        # Convert sources to tensor
+        sources = torch.from_numpy(sources)
+        if not self.return_id:
+            return mixture, sources
+        return mixture, sources, [row_speech['speech_ID'], row_music['music_ID']]
+
+    def get_infos(self):
+        """Get dataset infos (for publishing models).
+        Returns:
+            dict, dataset infos with keys `dataset`, `task` and `licences`.
+        """
+        infos = dict()
+        infos["dataset"] = self.dataset_name
+        return infos
--- create_dataset.py
+++ create_dataset.py
@@ -1,65 +1,49 @@
-# MiniLibriMix is a tiny version of LibriMix (https://github.com/JorisCos/LibriMix),
-# which is a free speech separation dataset.
-from asteroid.data import LibriMix
-
-# import musdb to create the mixtures: https://github.com/sigsep/sigsep-mus-db
-import musdb
-
-# Asteroid's System is a convenience wrapper for PyTorch-Lightning.
-from asteroid.engine import System
-
-from IPython.display import display, Audio
 import soundfile as sf
 import librosa, os
-# download the musdb library
-mus = musdb.DB(download=True)
-
-# This will automatically download MiniLibriMix from Zenodo on the first run.
-train_loader, val_loader = LibriMix.loaders_from_mini(task="sep_clean", batch_size=8)
-
+from os import listdir
+from os.path import isfile, join
+import random
+import numpy as np
+import re
+import csv
+# download the JamendoPopular library   
+# TODO wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=FILEID' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=FILEID" -O FILENAME && rm -rf /tmp/cookies.txt 
+# download the VCTK
+# TODO !wget --no-check-certificate 'https://datashare.ed.ac.uk/bitstream/handle/10283/3443/VCTK-Corpus-0.92.zip?sequence=2&isAllowed=y' -O 'VCTK-Corpus-0.92.zip'
 """
 Create the augmented dataset
-using the LibriMix and the Musdb18 datasets, an augmented 
+using the VCTK and the JamendoPopular datasets, an augmented 
 podcast/radioshow like dataset is created
 """
+# create files structure
+if not os.path.exists('podcastmix'):
+        os.makedirs('podcastmix')
 
 def create_folder_structure(path):
     if not os.path.exists(path):
         os.makedirs(path)
-    if not os.path.exists(path + '/linear_mono'):
-        os.makedirs(path + '/linear_mono')
-    if not os.path.exists(path + '/linear_stereo'):
-        os.makedirs(path + '/linear_stereo')
-    if not os.path.exists(path + '/sidechain_mono'):
-        os.makedirs(path + '/sidechain_mono')
-    if not os.path.exists(path + '/sidechain_stereo'):
-        os.makedirs(path + '/sidechain_stereo')
-    if not os.path.exists(path + '/track_mono'):
-        os.makedirs(path + '/track_mono')
-    if not os.path.exists(path + '/track_stereo'):
-        os.makedirs(path + '/track_stereo')
-    if not os.path.exists(path + '/speech_mono'):
-        os.makedirs(path + '/speech_mono')
+    if not os.path.exists(path + '/speech'):
+        os.makedirs(path + '/music')
 
 # create files structure
-train_path = "augmented_dataset/train"
+train_path = "podcastmix/train"
 create_folder_structure(train_path)
 
-val_path = "augmented_dataset/val"
+val_path = "podcastmix/val"
 create_folder_structure(val_path)
 
-test_path = "augmented_dataset/test"
+test_path = "podcastmix/test"
 create_folder_structure(test_path)
 
 # create the metadata directory
-if not os.path.exists('augmented_dataset/metadata'):
-    os.makedirs('augmented_dataset/metadata')
-if not os.path.exists('augmented_dataset/metadata/train'):
-    os.makedirs('augmented_dataset/metadata/train')
-if not os.path.exists('augmented_dataset/metadata/val'):
-    os.makedirs('augmented_dataset/metadata/val')
-if not os.path.exists('augmented_dataset/metadata/test'):
-    os.makedirs('augmented_dataset/metadata/test')
+if not os.path.exists('podcastmix/metadata'):
+    os.makedirs('podcastmix/metadata')
+if not os.path.exists('podcastmix/metadata/train'):
+    os.makedirs('podcastmix/metadata/train')
+if not os.path.exists('podcastmix/metadata/val'):
+    os.makedirs('podcastmix/metadata/val')
+if not os.path.exists('podcastmix/metadata/test'):
+    os.makedirs('podcastmix/metadata/test')
 
 from os import listdir
 from os.path import isfile, join
@@ -67,36 +51,6 @@ import random
 import numpy as np
 import re
 import csv
-
-def mix_audio_sources(track_path, speech_path, output_path, music_to_speech_ratio = 0.2):
-    """
-    Creates 4 mixes for the a music and a speech track and locates it in the output_path
-    the 4 mixes are: linear_mono, linear_stereo, sidechain_mono, sidechain_stereo
-    librimix is mono and musdb stereo
-    """
-    # read the files
-    track = librosa.load(track_path, sr=44100, mono=False)[0]
-    speech = librosa.load(speech_path, sr=44100)[0]
-    # match the length of the files
-    min_lenght = min(len(track[0]), len(speech))
-    
-    # crop the files to match in length
-    cropped_track_stereo = np.array([track[0][0:min_lenght], track[1][0:min_lenght]])
-    cropped_track_mono = cropped_track_stereo[0] + cropped_track_stereo[1]
-    cropped_speech = speech[0:min_lenght]
-    
-    linear_stereo = cropped_track_stereo * music_to_speech_ratio + cropped_speech
-    linear_mono = cropped_track_mono * music_to_speech_ratio + cropped_speech
-    
-    # write the files
-    file_name = re.sub("[^0-9a-zA-Z]+", "-", track_path.split('/')[-1]) + '_' + speech_path.split('/')[-1]
-    sf.write(output_path + "/linear_mono/" + file_name, linear_mono, 44100, subtype='PCM_24')
-    sf.write(output_path + "/linear_stereo/" + file_name, linear_stereo.T, 44100, subtype='PCM_24')
-    sf.write(output_path + "/speech_mono/" + file_name, cropped_speech, 44100, subtype='PCM_24')
-    sf.write(output_path + "/track_mono/" + file_name, cropped_track_mono, 44100, subtype='PCM_24')
-    sf.write(output_path + "/track_stereo/" + file_name, cropped_track_stereo.T, 44100, subtype='PCM_24')
-
-    return file_name, min_lenght
 
 # I used the s1 source from the MiniLibriMix for the train set
 # and the s3 source for the val and test set
@@ -113,104 +67,99 @@ random.shuffle(speech_array_train)
 random.shuffle(speech_array_val_test)
 
 
-# create the train csv file
-csv_path = 'augmented_dataset/metadata/train/linear_stereo.csv'
-with open(csv_path, 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(["","mixture_ID","mixture_path","track_path","speech_path","length"])
 
-csv_path = 'augmented_dataset/metadata/train/linear_mono.csv'
-with open(csv_path, 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(["","mixture_ID","mixture_path","track_path","speech_path","length"])
+def create_csv_metadata(csv_path, headers):
+    with open(csv_path, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(headers)
+# create the train csv file
+
+speech_headers = ["speech_ID","speeker_id","speech_path","length"]
+music_headers = ["music_ID", "music_path","length"]
+
+csv_path = 'podcastmix/metadata/train/speech.csv'
+create_csv_metadata(csv_path, speech_headers)
+
+csv_path = 'podcastmix/metadata/train/music.csv'
+create_csv_metadata(csv_path, music_headers)
 
 # create the val csv file
-csv_path = 'augmented_dataset/metadata/val/linear_stereo.csv'
-with open(csv_path, 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(["","mixture_ID","mixture_path","track_path","speech_path","length"])
+csv_path = 'podcastmix/metadata/val/speech.csv'
+create_csv_metadata(csv_path, speech_headers)
 
-csv_path = 'augmented_dataset/metadata/val/linear_mono.csv'
-with open(csv_path, 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(["","mixture_ID","mixture_path","track_path","speech_path","length"])
+csv_path = 'podcastmix/metadata/val/music.csv'
+create_csv_metadata(csv_path, music_headers)
 
 # create the test csv file
-csv_path = 'augmented_dataset/metadata/test/linear_stereo.csv'
-with open(csv_path, 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(["","mixture_ID","mixture_path","track_path","speech_path","length"])
+csv_path = 'podcastmix/metadata/test/speech.csv'
+create_csv_metadata(csv_path, speech_headers)
 
-csv_path = 'augmented_dataset/metadata/test/linear_mono.csv'
-with open(csv_path, 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(["","mixture_ID","mixture_path","track_path","speech_path","length"])
-
-# create the train/val sets:
+csv_path = 'podcastmix/metadata/test/music.csv'
+create_csv_metadata(csv_path, music_headers)
 
 # 70% train, 15 test 15 val
+# TODO I have to split the two datasets in the respective folder creating the 2 metadata files
 n_train = 102
 n_val_test = 21
 
+# speech_array_train = speech_array_train[0:n_train]
+# speech_array_val_test = speech_array_train[0: 2 * n_val_test]
+# print("speech_array_val_test", len(speech_array_val_test))
 
-
-speech_array_train = speech_array_train[0:n_train]
-speech_array_val_test = speech_array_train[0: 2 * n_val_test]
-print("speech_array_val_test", len(speech_array_val_test))
-
-i = 0
-for track in mus:
+# i = 0
+# for track in mus:
     
-    track_file = track.path
-    if i < n_train:
-        # creating the training set for the first n_train songs
-        csv_path = 'augmented_dataset/metadata/train'
-        speech_path = speech_path_train
-        path = train_path
-        speech_name = speech_array_train[i]
+#     track_file = track.path
+#     if i < n_train:
+#         # creating the training set for the first n_train songs
+#         csv_path = 'podcastmix/metadata/train'
+#         speech_path = speech_path_train
+#         path = train_path
+#         speech_name = speech_array_train[i]
         
-    else:
-        # the val/test set
-        if(i - n_train < n_val_test):
-            csv_path = 'augmented_dataset/metadata/val'
-            speech_path = speech_path_val_test
-            path = val_path
-        else:
-            csv_path = 'augmented_dataset/metadata/test'
-            speech_path = speech_path_val_test
-            path = test_path
-        print("i",i)
-        print("n_train", n_train)
-        print("//////////////////////////")
-        speech_name = speech_array_val_test[i - n_train - 1]
+#     else:
+#         # the val/test set
+#         if(i - n_train < n_val_test):
+#             csv_path = 'podcastmix/metadata/val'
+#             speech_path = speech_path_val_test
+#             path = val_path
+#         else:
+#             csv_path = 'podcastmix/metadata/test'
+#             speech_path = speech_path_val_test
+#             path = test_path
+#         print("i",i)
+#         print("n_train", n_train)
+#         print("//////////////////////////")
+#         speech_name = speech_array_val_test[i - n_train - 1]
 
-    # path of the speech
-    speech_file = speech_path + speech_name
-    file_name, min_length = mix_audio_sources(track_file, speech_file, path, music_to_speech_ratio = 0.1)
+#     # path of the speech
+#     speech_file = speech_path + speech_name
+#     file_name, min_length = mix_audio_sources(track_file, speech_file, path, music_to_speech_ratio = 0.1)
 
-    # add a new row to the corresponding csv metadata file
-    csv_path_file = csv_path + '/linear_mono.csv'
-    with open(csv_path_file, 'a', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow([
-                        i,
-                        file_name,
-                        path + "/linear_mono/" + file_name,
-                        path + "/track_mono/" + file_name,
-                        path + "/speech_mono/" + file_name,
-                        min_length
-            ])
+#     # add a new row to the corresponding csv metadata file
+#     csv_path_file = csv_path + '/speech.csv'
+#     with open(csv_path_file, 'a', newline='') as file:
+#         writer = csv.writer(file)
+#         writer.writerow([
+#                         i,
+#                         file_name,
+#                         path + "/linear_mono/" + file_name,
+#                         path + "/music_mono/" + file_name,
+#                         path + "/speech_mono/" + file_name,
+#                         min_length
+#             ])
+
+#     # add a new row to the corresponding csv metadata file
+#     csv_path_file = csv_path + '/music.csv'
+#     with open(csv_path_file, 'a', newline='') as file:
+#         writer = csv.writer(file)
+#         writer.writerow([
+#                         i,
+#                         file_name,
+#                         path + "/linear_mono/" + file_name,
+#                         path + "/music_mono/" + file_name,
+#                         path + "/speech_mono/" + file_name,
+#                         min_length
+#             ])
     
-    csv_path_file = csv_path + '/linear_stereo.csv'
-    with open(csv_path_file, 'a', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow([
-                        i,
-                        file_name,
-                        path + "/linear_stereo/" + file_name,
-                        path + "/track_stereo/" + file_name,
-                        path + "/speech_mono/" + file_name,
-                        min_length
-            ])
-    
-    i += 1
+#     i += 1
