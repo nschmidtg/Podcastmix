89d70bc6f55943e3a97274ad9f24c29735e67fee
Nicolas Schmidt <nschmidtg@gmail.com>
2021-03-26 23:39:26 +0100

    refactor(lint): added lint and changes in api call

--- search.py
+++ search.py
@@ -6,17 +6,21 @@ client_id = '08bac555'
 json_keys = {}
 last_offset = 1
 counter = 0
-for i in range(80):
-  offset = (i * 200)
-  r =requests.get('https://api.jamendo.com/v3.0/tracks/?client_id='+client_id+'&format=json&boost=downloads_total&limit=200&offset='+str(offset)+'&include=licenses')
-  songs = json.loads(r.text)['results']
-  for song in songs:
-    if(song['audiodownload_allowed']):
-      print('last_offset',last_offset)
-      print('i',i)
-      id = song['id']
-      json_keys[id] = song
-      last_offset += 1
+for i in range(70):
+    offset = (i * 200)
+    print(counter, '/', n_pages * 200)
+    r = requests.get(
+        'https://api.jamendo.com/v3.0/tracks/?client_id=' +
+        client_id +
+        '&format=json&order=popularity_total&audiodlformat='
+        'flac&limit=200&offset=0&include=musicinfo'
+    )
+    songs = json.loads(r.text)['results']
+    for song in songs:
+        if(song['audiodownload_allowed']):
+            id = song['id']
+            json_keys[id] = song
+            last_offset += 1
 
 file_name = 'metadata.json'
 with open(file_name, 'w') as outfile:
