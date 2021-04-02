333d3ec7461eeb5d07f70404cbcf5294bac11001
Nicolas <nschmidtg@gmail.com>
2021-04-02 13:38:43 +0200

    check with chanells

--- check_podcastmix_consistency.py
+++ check_podcastmix_consistency.py
@@ -7,7 +7,10 @@ import torchaudio
 
 def check_files_against_csv(csv_path, files_path, index_of_path_in_csv=7):
     not_missing = []
-
+    format_error = {}
+    format_error['sr'] = []
+    format_error['bits_per_sample'] = []
+    format_error['num_channels'] = []
     with open(csv_path, 'r') as read_obj:
         csv_reader = reader(read_obj)
         header = next(csv_reader)
@@ -17,10 +20,22 @@ def check_files_against_csv(csv_path, files_path, index_of_path_in_csv=7):
             for row in csv_reader:
                 # row variable is a list that represents a row in csv
                 path = row[index_of_path_in_csv]
+                # add to array to check metadata against list of files
                 if os.path.isfile(path):
                     not_missing.append(path.split('/')[3])
                 else:
                     print("im not file", path)
+                # check channels sr and bit depth
+                info = torchaudio.info(path)
+                print(info.sample_rate, info.bits_per_sample, info.num_channels)
+                if(not info.sample_rate == 44100):
+                    format_error['sr'].append(path)
+                if(not info.bits_per_sample == 16):
+                    format_error['bits_per_sample'].append(path)
+                if(('music' in path) and (not info.num_channels == 2)):
+                    format_error['num_channels_music'].append(path)
+                if(('speech' in path) and (not info.num_channels == 1)):
+                    format_error['num_channels_speech'].append(path)
 
     onlyfiles = [f for f in listdir(files_path) if isfile(join(files_path, f))]
     print('Check diff between:', files_path, csv_path)
@@ -29,7 +44,7 @@ def check_files_against_csv(csv_path, files_path, index_of_path_in_csv=7):
     diff2 = list(set(not_missing) - set(onlyfiles))
     print('List in csv minus list of files:', len(diff2))
 
-
+    print('format errors:', format_error)
 # check consistency of the dataset:
 files_path = 'podcastmix/test/music'
 csv_path = 'podcastmix/metadata/test/music.csv'
