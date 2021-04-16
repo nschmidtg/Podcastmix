32f69d968da8b3809286821b068aa86ac31ab998
Nicolas Schmidt <nschmidtg@gmail.com>
2021-04-16 11:23:48 +0200

    gitignore and install dependencies

---
+++ install_dependencies.sh
@@ -0,0 +1,6 @@
+# First off, install asteroid
+pip install git+https://github.com/nschmidtg/asteroid
+# and pytorch-lightning
+pip install pytorch-lightning
+# install ffmpeg (stems are mp4 by default)
+sudo apt-get install ffmpeg
