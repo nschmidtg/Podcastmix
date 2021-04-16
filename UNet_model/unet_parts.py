f485b8b3b44912b985e4d08d338011cc99afd6e4
Nicolas <nschmidtg@gmail.com>
2021-04-16 12:08:48 +0200

    passing concatenation

--- UNet_model/unet_parts.py
+++ UNet_model/unet_parts.py
@@ -58,6 +58,15 @@ class up(nn.Module):
         )
 
     def forward(self, x1, x2):
+        # input is CHW
+        diffY = x2.size()[2] - x1.size()[2]
+        diffX = x2.size()[3] - x1.size()[3]
+
+        x1 = F.pad(x1, (diffX // 2, diffX - diffX//2,
+                        diffY // 2, diffY - diffY//2))
+
+        print("x1:", x1.shape)
+        print("x2:", x2.shape)
         x = torch.cat([x2, x1], dim=1)
         x = self.deconv(x)
         return x
