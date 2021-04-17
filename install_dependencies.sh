0d24c6c29d410addb9a9f1d47a931eb405566d5a
Nicolas Schmidt <nschmidtg@gmail.com>
2021-04-17 19:02:04 +0200

    feat(unet): update README.md

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,6 +1,3 @@
-# First off, install asteroid
-pip install git+https://github.com/nschmidtg/asteroid
-# and pytorch-lightning
+pip install git+https://github.com/asteroid-team/asteroid
 pip install pytorch-lightning
-# install ffmpeg (stems are mp4 by default)
-sudo apt-get install ffmpeg
+conda install -c conda-forge ffmpeg
