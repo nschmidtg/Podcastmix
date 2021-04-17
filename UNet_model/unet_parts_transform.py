a9214fd4beb8a3624e819acf379c075ae185d952
Nicolas Schmidt <nschmidtg@gmail.com>
2021-04-17 18:38:27 +0200

    feat(unet): comments and delete prints

--- UNet_model/unet_parts_transform.py
+++ UNet_model/unet_parts_transform.py
@@ -3,8 +3,6 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
-
 class down(nn.Module):
     '''(conv => BN => LeackyReLU)'''
     def __init__(self, in_ch, out_ch, kernel_size, stride):
@@ -18,8 +16,12 @@ class down(nn.Module):
     def forward(self, x):
         x = self.conv(x)
         return x
-
 class up(nn.Module):
+    '''
+        (concatenation => deconv => BN => ReLU => Dropout )
+        as proposed in SINGING VOICE SEPARATION WITH DEEP
+        U-NET CONVOLUTIONAL NETWORK
+    '''
     def __init__(self, in_ch, out_ch, kernel_size, stride):
         super(up, self).__init__()
         self.deconv = nn.Sequential(
@@ -30,72 +32,15 @@ class up(nn.Module):
         )
 
     def forward(self, x1, x2):
-        # input is CHW
+        # input is CHW. Taken from 
+        # https://github.com/Steve-Tod/Audio-source-separation-with-Unet/blob/master/models/unet/unet_parts.py
         diffY = x2.size()[2] - x1.size()[2]
         diffX = x2.size()[3] - x1.size()[3]
 
         x1 = F.pad(x1, (diffX // 2, diffX - diffX//2,
                         diffY // 2, diffY - diffY//2))
 
-        print("x1:", x1.shape)
-        print("x2:", x2.shape)
         x = torch.cat([x2, x1], dim=1)
-        print("x_ before deconv:", x.shape)
         x = self.deconv(x)
-        print("x_after devonc:", x.shape)
-        return x
 
-# class down(nn.Module):
-#     def __init__(self, in_ch, out_ch):
-#         super(down, self).__init__()
-#         self.mpconv = nn.Sequential(
-#             nn.MaxPool2d(2),
-#             double_conv(in_ch, out_ch)
-#         )
-
-#     def forward(self, x):
-#         x = self.mpconv(x)
-#         return x
-
-
-# class up(nn.Module):
-#     def __init__(self, in_ch, out_ch, bilinear=False):
-#         super(up, self).__init__()
-
-#         #  would be a nice idea if the upsampling could be learned too,
-#         #  but my machine do not have enough memory to handle all those weights
-#         if bilinear:
-#             #self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-#             self.bilinear = True
-#         if not bilinear:
-#             self.bilinear = False
-#             self.up = nn.ConvTranspose2d(in_ch//2, in_ch//2, 2, stride=2)
-
-#         self.conv = double_conv(in_ch, out_ch)
-
-#     def forward(self, x1, x2):
-#         if self.bilinear:
-#             x1 = F.interpolate(x1, scale_factor=2, mode='bilinear', align_corners=False)
-#         else:
-#             x1 = self.up(x1)
-        
-#         # input is CHW
-#         diffY = x2.size()[2] - x1.size()[2]
-#         diffX = x2.size()[3] - x1.size()[3]
-
-#         x1 = F.pad(x1, (diffX // 2, diffX - diffX//2,
-#                         diffY // 2, diffY - diffY//2))
-
-#         x = torch.cat([x2, x1], dim=1)
-#         x = self.conv(x)
-#         return x
-
-
-class outconv(nn.Module):
-    def __init__(self, in_ch, out_ch):
-        super(outconv, self).__init__()
-        self.conv = nn.Conv2d(in_ch, out_ch, 1)
-
-    def forward(self, x):
-        x = self.conv(x)
         return x
