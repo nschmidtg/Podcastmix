314a9c8245a6dfc7f76ac6e87b1c4afe1b7a87be
Nicolas <nschmidtg@gmail.com>
2021-04-26 16:15:37 +0200

    try with more filters U-Net

--- UNet_model/unet_model_transform.py
+++ UNet_model/unet_model_transform.py
@@ -11,39 +11,37 @@ from asteroid.filterbanks import make_enc_dec
 
 class UNet(BaseModel):
     #def __init__(self, n_channels, n_classes, bilinear=True):
-    def __init__(self, segment, sample_rate, fft_size, hop_size, window_size, kernel_size_c, stride_c, kernel_size_d, stride_d):
+    def __init__(self, segment, sample_rate, fft_size, hop_size, window_size, kernel_size, stride):
         super(UNet, self).__init__()
         self.segment = segment
         self.sample_rate = sample_rate
         self.window_size = window_size
         self.fft_size = fft_size
         self.hop_size = hop_size
-        self.kernel_size_c = kernel_size_c
-        self.stride_c = stride_c
-        self.kernel_size_d = kernel_size_d
-        self.stride_d = stride_d
+        self.kernel_size = kernel_size
+        self.stride = stride
 
         # declare layers
-        self.down1 = down(1, 16, self.kernel_size_c, self.stride_c)
-        self.down2 = down(16, 32, self.kernel_size_c, self.stride_c)
-        self.down3 = down(32, 64, self.kernel_size_c, self.stride_c)
-        self.down4 = down(64, 128, self.kernel_size_c, self.stride_c)
-        self.down5 = down(128, 256, self.kernel_size_c, self.stride_c)
-        self.down6 = down(256, 512, self.kernel_size_c, self.stride_c)
+        self.down1 = down(1, 16, self.kernel_size, self.stride)
+        self.down2 = down(16, 32, self.kernel_size, self.stride)
+        self.down3 = down(32, 64, self.kernel_size, self.stride)
+        self.down4 = down(64, 128, self.kernel_size, self.stride)
+        self.down5 = down(128, 256, self.kernel_size, self.stride)
+        self.down6 = down(256, 512, self.kernel_size, self.stride)
 
 
-        self.up1 = up(512, 256, self.kernel_size_d, self.stride_d, (0,0), 1)
-        self.up2 = up(256, 128, self.kernel_size_d, self.stride_d, (0,1), 2)
-        self.up3 = up(128, 64, self.kernel_size_d, self.stride_d, (0,1), 3)
-        self.up4 = up(64, 32, self.kernel_size_d, self.stride_d, (1,0), 4)
-        self.up5 = up(32, 16, self.kernel_size_d, self.stride_d, (0,0), 5)
-        self.last_layer = last_layer(16, 1, self.kernel_size_d, self.stride_d, (1, 1))
+        self.up1 = up(512, 256, self.kernel_size, self.stride, (0,0), 1)
+        self.up2 = up(256, 128, self.kernel_size, self.stride, (0,1), 2)
+        self.up3 = up(128, 64, self.kernel_size, self.stride, (0,1), 3)
+        self.up4 = up(64, 32, self.kernel_size, self.stride, (1,0), 4)
+        self.up5 = up(32, 16, self.kernel_size, self.stride, (0,0), 5)
+        self.last_layer = last_layer(16, 1, self.kernel_size, self.stride, (1, 1))
 
         # Create STFT/iSTFT pair in one line
         self.stft, self.istft = make_enc_dec(
             'stft',
-            n_filters=1024,
-            kernel_size=self.fft_size,
+            n_filters=self.fft_size,
+            kernel_size=self.window_size,
             stride=self.hop_size
         )
 
@@ -141,9 +139,7 @@ class UNet(BaseModel):
             "fft_size": self.fft_size,
             "hop_size": self.hop_size,
             "window_size": self.window_size,
-            "kernel_size_c": self.kernel_size_c,
-            "stride_c": self.stride_c,
-            "kernel_size_d": self.kernel_size_d,
-            "stride_d": self.stride_d
+            "kernel_size": self.kernel_size,
+            "stride": self.stride
         }
         return model_args
