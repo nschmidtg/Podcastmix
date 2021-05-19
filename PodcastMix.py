c3afab62ee190044c8b49b62087baa18c46ee685
Nicolas <nschmidtg@gmail.com>
2021-05-19 15:30:37 +0200

    ddp batches

--- PodcastMix.py
+++ PodcastMix.py
@@ -59,7 +59,7 @@ class PodcastMix(Dataset):
 
     def __len__(self):
         # for now, its a full permutation
-        return 100
+        return 1000
         return min([len(self.df_speech), len(self.df_music)])
 
 
