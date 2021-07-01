3545a316714922fbc77919f9fb2842410bdb5db9
Nicolas Schmidt <nschmidtg@gmail.com>
2021-07-01 11:16:33 +0200

    added log2spec, still trying

--- logl2.py
+++ logl2.py
@@ -15,3 +15,18 @@ class LogL2Time(_Loss):
         loss = 10 / (number_of_sources * length_of_sources) * sum_of_log_of_previous
         loss = loss.mean(dim=0)
         return loss
+
+class LogL2Spec(_Loss):
+    
+    def forward(self, est_targets, targets):
+        if targets.size() != est_targets.size() or targets.ndim < 2:
+            raise TypeError(
+                f"Inputs must be of shape [batch, *], got {targets.size()} and {est_targets.size()} instead"
+            )
+        batch, number_of_sources, n_bins, n_frames = est_targets.shape
+        squared_abs_dif = torch.abs((est_targets - targets) ** 2)
+        sum_of_squared_abs_dif = torch.sum(squared_abs_dif, dim=2)
+        sum_of_log_of_previous = torch.sum(torch.log10(sum_of_squared_abs_dif), dim=1)
+        loss = 10 / (number_of_sources * n_bins * n_frames) * sum_of_log_of_previous
+        loss = loss.mean(dim=0)
+        return loss
