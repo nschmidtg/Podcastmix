a976376ad2be477a58f3b90b78ed37130c0bd98a
Marius Miron <miron.marius@gmail.com>
2021-09-15 18:56:08 +0200

    add script

---
+++ _temp.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+CUDA_VISIBLE_DEVICES=1 python test_real.py --target_model ConvTasNet --test_dir podcastmix/real_podcasts/metadata --out_dir=/home/marius/data/Podcastmix/real/ConvTasNet --exp_dir=/mnt/sda1/nicolas/tesis/thesis/ConvTasNet_model/exp/tmp --use_gpu=1 --n_save_ex=-1
+CUDA_VISIBLE_DEVICES=1 python test_real.py --target_model UNet --test_dir podcastmix/real_podcasts/metadata --out_dir=/home/marius/data/Podcastmix/real/UNet --exp_dir=/mnt/sda1/nicolas/tesis/thesis/UNet_model/exp/tmp --use_gpu=1 --n_save_ex=-1
+CUDA_VISIBLE_DEVICES=1 python test.py --target_model ConvTasNet --test_dir podcastmix/real_podcasts/metadata --out_dir=/home/marius/data/Podcastmix/synth/ConvTasNet --exp_dir=/mnt/sda1/nicolas/tesis/thesis/ConvTasNet_model/exp/tmp --use_gpu=1 --n_save_ex=-1
+CUDA_VISIBLE_DEVICES=1 python test.py --target_model UNet --test_dir podcastmix/real_podcasts/metadata --out_dir=/home/marius/data/Podcastmix/synth/UNet --exp_dir=/mnt/sda1/nicolas/tesis/thesis/UNet_model/exp/tmp --use_gpu=1 --n_save_ex=-1
