fix averaging bugs

test_real.py
@@ -192,7 +192,7 @@ def main(conf):
                     est_src,
                     conf["sample_rate"],
                 )
-        #import pdb;pdb.set_trace()
+
         # Write local metrics to the example folder.
         with open(local_save_dir + "metrics.json", "w") as f:
             json.dump({k:v.tolist() for k,v in utt_metrics.items()}, f, indent=0)
@@ -221,7 +221,7 @@ def main(conf):
             f.write(wer_card)
 
     with open(os.path.join(eval_save_dir, "final_metrics.json"), "w") as f:
-        json.dump(final_results, f, indent=0)
+        json.dump({k:v.tolist() for k,v in final_results.items()}, f, indent=0)
 
     # for publishing the model:
     # model_dict = torch.load(model_path, map_location="cpu")