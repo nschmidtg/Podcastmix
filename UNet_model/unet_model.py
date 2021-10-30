29f34fc2902b50a0811f67d97416c2138eb047c2
Nicolas Schmidt <nschmidtg@gmail.com>
2021-10-30 13:25:41 +0200

    cuda available check

--- UNet_model/unet_model.py
+++ UNet_model/unet_model.py
@@ -61,7 +61,8 @@ class UNet(BaseModel):
         mean = torch.mean(x_in)
         std = torch.std(x_in)
         x_in = (x_in - mean) / (1e-5 + std)
-        x_in = x_in.cuda()
+        if torch.cuda.is_available():
+            x_in = x_in.cuda()
 
         # compute normalized spectrogram
         window = torch.hamming_window(self.window_size, device='cuda')
